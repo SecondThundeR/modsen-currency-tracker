2a014310fdaa29a5472cdd65820f1d9a4b1bb44f
SecondThundeR <awayfromgalaxy@gmail.com>
2023-09-02 00:43:48 +0300

    refactor: replace logo with upscaled version

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import headerLogo from "@/assets/headerLogo.png";
+import diagramLogo from "@/assets/diagramLogo.png";
 
 import styles from "./Header.module.css";
 
 function Header() {
   return (
     <header className={styles["Header"]}>
-      <img src={headerLogo} />
+      <img width={36} height={36} src={diagramLogo} />
       <nav className={styles["Navigation"]}>
         <Link className={styles["Link"]} to="/">
           Home

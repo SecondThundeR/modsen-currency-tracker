23c977ca14147c577951fd9a1584de0193adf1d4
SecondThundeR <awayfromgalaxy@gmail.com>
2023-09-01 23:37:11 +0300

    feat: add RouterProvider support

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { RouterProvider } from "react-router-dom";
 
-import Counter from "@/components/Counter/Counter";
+import { router } from "./routes";
 
 function App() {
-  return <Counter />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;

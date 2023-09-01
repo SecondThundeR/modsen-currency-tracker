fcbfe8669edb827c2f5448db5eaf9fa97dd4de70
SecondThundeR <awayfromgalaxy@gmail.com>
2023-09-01 23:36:50 +0300

    feat: add basic router with placeholder routes

---
+++ src/components/Root/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+function Root() {
+  return (
+    <div>
+      <h1>Root!</h1>
+      <Outlet />
+    </div>
+  );
+}
+
+export default Root;

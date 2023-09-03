eae3b2d1deb9a7aead3da86d09b6b809e4b51e1b
SecondThundeR <awayfromgalaxy@gmail.com>
2023-09-03 12:57:25 +0300

    refactor(components): update Footer imports

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,8 @@
 import React, { memo } from "react";
 
 import diagramLogo from "@/assets/diagramLogo.png";
+import Icon from "@/components/Icon";
 
-import Icon from "../Icon";
 import styles from "./Footer.module.css";
 
 const Footer = memo(function Footer() {

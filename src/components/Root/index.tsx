4cfc033b14a637e671442d7731b212582a48d6a4
SecondThundeR <awayfromgalaxy@gmail.com>
2023-09-03 12:52:15 +0300

    feat(components): add Footer component

--- src/components/Root/index.tsx
+++ src/components/Root/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
-import Header from "../Header";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 
 function Root() {
   return (
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }

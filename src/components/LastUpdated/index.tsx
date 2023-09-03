ba48228b1233bceaad6bb777cbbd345f55fc00e7
SecondThundeR <awayfromgalaxy@gmail.com>
2023-09-03 16:19:16 +0300

    feat: add LastUpdated component

---
+++ src/components/LastUpdated/index.tsx
@@ -0,0 +1,20 @@
+import React, { memo } from "react";
+
+import styles from "./LastUpdated.module.css";
+
+interface LastUpdatedProps {
+  updateTime: string;
+}
+
+const LastUpdated = memo(function LastUpdated({
+  updateTime,
+}: LastUpdatedProps) {
+  return (
+    <div className={styles["LastUpdated"]}>
+      <div className={styles["LastUpdated__Animation"]} />
+      <p>Last updated at {updateTime}</p>
+    </div>
+  );
+});
+
+export default LastUpdated;
--- src/components/Root/index.tsx
+++ src/components/Root/index.tsx
@@ -3,14 +3,15 @@ import { Outlet } from "react-router-dom";
 
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-
-import HeroBlock from "../HeroBlock";
+import HeroBlock from "@/components/HeroBlock";
+import LastUpdated from "@/components/LastUpdated";
 
 function Root() {
   return (
     <div>
       <Header />
       <HeroBlock />
+      <LastUpdated updateTime="12:00 pm" />
       <Outlet />
       <Footer />
     </div>

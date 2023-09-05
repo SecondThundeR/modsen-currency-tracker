d00e7d28a08a4fb47ee7a07adb8964b865770d7a
SecondThundeR <awayfromgalaxy@gmail.com>
2023-09-05 16:51:05 +0300

    feat(components): update Home component
    Add support for `useCurrencyModal`
    Add support for `onSectionClick`
    Add CurrencyModal on page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -4,12 +4,19 @@ import CurrencyGrid from "@/components/ui/CurrencyGrid";
 import Loader from "@/components/ui/Loader";
 import PlaceholderHero from "@/components/ui/PlaceholderHero";
 import { PLACEHOLDER_STOCKS_DATA } from "@/constants/placeholderStocks";
+import useCurrencyModal from "@/hooks/useCurrencyModal";
 import useRates from "@/hooks/useRates";
 import { mergeOptionsWithRates } from "@/utils/mergeOptionsWithRates";
 
+import CurrencyModal from "../ui/CurrencyModal";
 import styles from "./Home.module.css";
 
 function Home() {
+  const {
+    isOpen,
+    selectedId,
+    handlers: { onOpen, onClose },
+  } = useCurrencyModal();
   const rates = useRates("USD");
   const sectionData = useMemo(
     () => mergeOptionsWithRates(rates.currentRates),
@@ -20,15 +27,18 @@ function Home() {
 
   if (rates.isError) return <PlaceholderHero text="No data!" />;
 
-  console.log(rates.currentRates);
-
   return (
     <div className={styles["Home"]}>
       <CurrencyGrid
         sectionTitle="Stocks"
         sectionInfo={PLACEHOLDER_STOCKS_DATA}
       />
-      <CurrencyGrid sectionTitle="Quotes" sectionInfo={sectionData} />
+      <CurrencyGrid
+        sectionTitle="Quotes"
+        sectionInfo={sectionData}
+        onSectionClick={onOpen}
+      />
+      {isOpen && <CurrencyModal selectedId={selectedId} closeModal={onClose} />}
     </div>
   );
 }

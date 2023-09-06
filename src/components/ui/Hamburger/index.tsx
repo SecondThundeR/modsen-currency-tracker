af62b2f91bcc79a1ced2986562dc85d9f3e574e7
SecondThundeR <awayfromgalaxy@gmail.com>
2023-09-06 18:47:10 +0300

    feat(components): add basic `Hamburger` component
    Component is fully controllable and has animation when toggling states (open -> close -> ...)

---
+++ src/components/ui/Hamburger/index.tsx
@@ -0,0 +1,36 @@
+import React, { memo } from "react";
+import clsx from "clsx";
+
+import styles from "./Hamburger.module.css";
+
+interface HamburgerProps {
+  isOpen: boolean;
+  toggleOpen: () => void;
+}
+
+const Hamburger = memo(function Hamburger({
+  isOpen,
+  toggleOpen,
+}: HamburgerProps) {
+  return (
+    <div className={styles["Hamburger"]} onClick={toggleOpen}>
+      <span
+        className={clsx(styles["Hamburger__Burger"], {
+          [styles["Hamburger__Burger--first"]]: isOpen,
+        })}
+      />
+      <span
+        className={clsx(styles["Hamburger__Burger"], {
+          [styles["Hamburger__Burger--second"]]: isOpen,
+        })}
+      />
+      <span
+        className={clsx(styles["Hamburger__Burger"], {
+          [styles["Hamburger__Burger--third"]]: isOpen,
+        })}
+      />
+    </div>
+  );
+});
+
+export default Hamburger;

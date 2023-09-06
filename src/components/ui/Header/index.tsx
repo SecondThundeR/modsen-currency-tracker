3b27877f1c3e0023476d66776a61386b027b0d1b
SecondThundeR <awayfromgalaxy@gmail.com>
2023-09-06 18:47:56 +0300

    feat(components): update `Header` component
    Add support for Hamburger component
    Slightly updated layout structure to support hamburger-like functionality

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -1,23 +1,39 @@
 import React from "react";
+import clsx from "clsx";
 
 import diagramLogo from "@/assets/diagramLogo.png";
 import Button from "@/components/ui/Button";
+import Hamburger from "@/components/ui/Hamburger";
 import Icon from "@/components/ui/Icon";
 import NavMenu from "@/components/ui/NavMenu";
 import { NAV_MENU_ITEMS } from "@/constants/navMenuItems";
+import useBooleanToggle from "@/hooks/useBooleanToggle";
 
 import styles from "./Header.module.css";
 
 function Header() {
+  const [isOpen, toggleIsOpen] = useBooleanToggle();
+
   return (
     <header className={styles["Header"]}>
-      <Icon width={36} height={36} iconSrc={diagramLogo} />
-      <NavMenu
-        navClass={styles["Navigation"]}
-        linkClass={styles["Link"]}
-        menu={NAV_MENU_ITEMS}
-      />
-      <Button>Change theme</Button>
+      <div className={styles["Header__Burger"]}>
+        <Icon width={36} height={36} iconSrc={diagramLogo} />
+        <div className={styles["Header__BurgerWrapper"]}>
+          <Hamburger isOpen={isOpen} toggleOpen={toggleIsOpen} />
+        </div>
+      </div>
+      <div
+        className={clsx(styles["Header__Content"], {
+          [styles["Header__Content--visible"]]: isOpen,
+        })}
+      >
+        <NavMenu
+          navClass={styles["Navigation"]}
+          linkClass={styles["Link"]}
+          menu={NAV_MENU_ITEMS}
+        />
+        <Button>Change theme</Button>
+      </div>
     </header>
   );
 }

0c5e6bfc2867ae86bba4c4b7df58585de6e3b654
SecondThundeR <awayfromgalaxy@gmail.com>
2023-09-08 18:13:47 +0300

    feat(utils): add `extractChartData` util for converting API data to chart data

---
+++ src/utils/extractChartData.ts
@@ -0,0 +1,19 @@
+import { RateHistory } from "@/types/coinapi";
+
+export function extractChartData(data: RateHistory[]) {
+  return data
+    .map((item) => {
+      const { time_period_start, rate_open, rate_high, rate_low, rate_close } =
+        item;
+      return {
+        x: new Date(time_period_start),
+        y: [
+          rate_open.toFixed(2),
+          rate_high.toFixed(2),
+          rate_low.toFixed(2),
+          rate_close.toFixed(2),
+        ],
+      };
+    })
+    .reverse();
+}

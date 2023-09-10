32f91cf7f8f94f1ab6fdd24186a46d0a312aaa99
SecondThundeR <awayfromgalaxy@gmail.com>
2023-09-10 21:16:25 +0300

    feat(routes): add support for contact route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 
+import Contact from "@/components/Contact";
 import Home from "@/components/Home";
 import InvalidPage from "@/components/InvalidPage";
 import Root from "@/components/Root";
 import Timeline from "@/components/Timeline";
-import { INVALID_ROUTE, ROOT_ROUTE, TIMELINE_ROUTE } from "@/constants/routes";
+import {
+  CONTACT_ROUTE,
+  INVALID_ROUTE,
+  ROOT_ROUTE,
+  TIMELINE_ROUTE,
+} from "@/constants/routes";
 
 export const router = createBrowserRouter([
   {
@@ -19,6 +25,10 @@ export const router = createBrowserRouter([
       {
         path: TIMELINE_ROUTE,
         element: <Timeline />,
+      },
+      {
+        path: CONTACT_ROUTE,
+        element: <Contact />,
       },
       {
         path: INVALID_ROUTE,

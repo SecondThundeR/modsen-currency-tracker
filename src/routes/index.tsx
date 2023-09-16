115909402b7eb235a203fbf87bdcd9e84bdff68e
SecondThundeR <awayfromgalaxy@gmail.com>
2023-09-16 11:01:06 +0300

    fix(routes): update path to `InvalidPage`

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,9 +4,9 @@ import { createBrowserRouter } from "react-router-dom";
 import BankMap from "@/components/BankMap";
 import Contact from "@/components/Contact";
 import Home from "@/components/Home";
-import InvalidPage from "@/components/InvalidPage";
 import Root from "@/components/Root";
 import Timeline from "@/components/Timeline";
+import InvalidPage from "@/components/ui/InvalidPage";
 import {
   BANK_MAP_ROUTE,
   CONTACT_ROUTE,
